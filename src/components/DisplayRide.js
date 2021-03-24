8892d27ebb4454c5990f9dd0100fec15b453f4eb
LeGrumph <57810002+LeGrumph@users.noreply.github.com>
2021-03-24 19:39:28 +0100

    modify li to div in DisplayRide

--- src/components/DisplayRide.js
+++ src/components/DisplayRide.js
@@ -5,12 +5,12 @@ const DisplayRide = (props)  => {
   return (
      <div>
      {props.taxis.map((item) => (
-        <li key ={item.id}>
+        <div key ={item.id}>
           <Ride id={item.id} 
                 distance={item.distance}
                 duration={item.duration} 
                 startTime={item.startTime}/>
-        </li>
+        </div>
         ))}
      </div>
   )
